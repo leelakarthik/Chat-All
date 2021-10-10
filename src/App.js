84087ec69d23a69a5a3fe3ee2dd2439524e8d391
Leela Karthik <karthik.uttarkar10@gmail.com>
2021-10-10 20:50:51 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+import Form from "./components/form";
+import Modal from "./Modal";
+// import { getMsgs } from "./service";
+
+const connection = io.connect("http://localhost:3001");
+connection.on("welcome", (data) => {
+  console.log("welcome user " + data);
+});
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const setRoomValue = (data) => {
+    console.log("In Room ", data);
+    if (data !== "") {
+      connection.emit("join_room", data);
+      setVisible(!visible);
+    }
+  };
+  return (
+    <React.Fragment>
+      {!visible && <Modal callback={setRoomValue} />}
+      {visible && (
+        <div className="container" id="main">
+          <div className="row">
+            <h3 id="title">Ant powered Chat Application</h3>
+          </div>
+          <Form connection={connection} />
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default App;
